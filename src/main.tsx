02e1b2a374872cbac03394be096e1c1df69496d5
cryptomemeinsight <cryptoinsightbyai@gmail.com>
2025-12-29 21:25:59 +0545

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
     WalletModalProvider,
 } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
